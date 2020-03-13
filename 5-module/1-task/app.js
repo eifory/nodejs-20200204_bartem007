0814505bc1f770af880c7aa26a4f26c7d9826c3d
eifory <bartem007@gmail.com>
2020-03-14 00:13:15 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,24 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const waitForMessage = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((res, rej) => {
+    waitForMessage.push(res);
+  });
+  ctx.body = await promise;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (message !== undefined && message !== '') {
+    waitForMessage.forEach((awaiting) => {
+      awaiting(message);
+    });
+  }
+  ctx.status = 200;
+  ctx.res.end();
 });
 
 app.use(router.routes());

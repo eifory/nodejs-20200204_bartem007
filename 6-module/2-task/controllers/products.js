9949e35676d571461c797e23e39cb43af83e7241
eifory <bartem007@gmail.com>
2020-05-11 22:51:25 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,45 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
-};
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+const formatProduct = require('../format/formatProduct');
+
+// module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
+//   const subcategory = ctx.query.subcategory;
+//   const products = await Product.find({
+//     subcategory: subcategory,
+//   });
+//   ctx.body = {
+//     products: products.map(formatProduct),
+//   };
+// };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  if (ctx.query.subcategory) {
+    const subcategory = ctx.query.subcategory;
+    const products = await Product.find({
+      subcategory: subcategory,
+    });
+    ctx.body = {
+      products: products.map(formatProduct),
+    };
+  } else {
+      const products = await Product.find({});
+      ctx.body = {
+        products: products.map(formatProduct),
+      };
+  }
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const productId = ctx.params.id;
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    ctx.throw(400, 'id is not valid');
+  }
+  const product = await Product.findById(productId);
+  if (!product) {
+    ctx.throw(404, 'there is no product with this id');
+  }
+  ctx.body = {
+    product: formatProduct(product),
+  };
 };
 

d603b42ae023930399c18eb4e28d32f9eb10bc0f
eifory <bartem007@gmail.com>
2020-07-02 16:18:46 +0300

    for A.Griadchenko review

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,14 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message')
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+
+  const userId = ctx.user.id;
+
+  
+  const messages = await Message.find({chat: userId})
+    .sort({date: 1})
+    .limit(20);
+  console.log(messages);
+  ctx.body = {messages: messages.map(mapMessage)};
 };
